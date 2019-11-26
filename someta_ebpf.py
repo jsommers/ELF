9cda43add9ec432b851baed1b876abf9e891694f
jsommers <jsommers@colgate.edu>
2019-11-26 16:06:57 -0500

    refactoring python control program

--- someta_ebpf.py
+++ someta_ebpf.py
@@ -24,15 +24,13 @@ class ProbeConfig(object):
         self.bunch = args.bunch
         self.debug = args.debug
         self.interval = args.spacing
-        self.mode = args.mode
         self.device = args.device
-        self.host = args.host
         self.maxttl = args.maxttl
         self.hostnames = args.hostnames
         self.status = 1.0
         self.ether = args.noether
         self.outfile = args.outfile
-        self.cmd = args.cmd
+        self.code = args.code
         self.timeout = int(args.timeout)
         self._sanitycheck()
 
@@ -41,26 +39,16 @@ class ProbeConfig(object):
             raise InvalidConfiguation("fail: interval is less than 10 microsec")
         if self.device is None:
             raise InvalidConfiguation("Need a device")
-        if self.cmd and not self.host:
-            raise InvalidConfiguration("No host specified for command")
-        if not self.host and not self.hostnames:
+        if not self.code:
+            raise InvalidConfiguration("No eBPF code file specified")
+        if not self.hostnames:
             raise InvalidConfiguration("No hosts specified for tracing")
-        if self.mode not in ('icmp','tcp'):
-            raise InvalidConfiguration("Invalid mode")
+        if self.maxttl < 1 or self.maxttl > 64:
+            raise InvalidConfiguration("Invalid maxttl setting")
 
     def cflags(self):
-        return ["-DDEBUG={}".format(int(self.debug)), "-DPROBE_INTERVAL={}".format(self.interval), "-DBUNCH={}".format(int(self.bunch)), "-DICMPMODE={}".format(int(self.mode=='icmp')), "-DTCPMODE={}".format(int(self.mode=='tcp')), "-DETHER_ENCAP={}".format(int(self.ether))]
+        return ["-DDEBUG={}".format(int(self.debug)), "-DPROBE_INTERVAL={}".format(self.interval), "-DBUNCH={}".format(int(self.bunch)), "-DETHER_ENCAP={}".format(int(self.ether))]
 
-
-def get_hops(host):
-    cmd = ['/usr/sbin/traceroute', '-n', host]
-    p = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    stdout, stderr = p.communicate()
-    lines = stdout.split('\n')
-    for i in range(len(lines)-1, -1, -1):
-        if not lines[i].strip():
-            continue
-        return int(lines[i].split()[0])
 
 def lookup_host(host, alllist):
     try:
@@ -90,13 +78,10 @@ def main(config):
     logging.info("ifindex for {} is {}".format(config.device, idx))
 
     iplist = []
-    lookup_host(config.host, iplist)
     for h in config.hostnames:
         lookup_host(h, iplist)
     if config.maxttl > 0:
         numhops = config.maxttl
-    else:
-        numhops = get_hops(iplist[0][0])
     logging.info("{} is {} hops away".format(config.host, numhops))
 
     debugflag = 0     
@@ -106,9 +91,9 @@ def main(config):
     cflags=["-Wall", "-DIFINDEX={}".format(idx), "-DNUM_HOPS={}".format(numhops-1)]
     cflags += config.cflags()
 
-    ibpf = bcc.BPF(src_file='inband_test.c', debug=debugflag, cflags=cflags)
+    ibpf = bcc.BPF(src_file=config.code, debug=debugflag, cflags=cflags)
     ingress_fn = ibpf.load_func("ingress_path", bcc.BPF.XDP)
-    egress_fn = ibpf.load_func('egress_path_{}'.format(config.mode), bcc.BPF.SCHED_CLS)
+    egress_fn = ibpf.load_func('egress_path'.format(config.mode), bcc.BPF.SCHED_CLS)
     ibpf.attach_xdp(config.device, ingress_fn)
 
     try:
@@ -126,14 +111,6 @@ def main(config):
         logging.info("Adding {} to ip interest hash".format(ipstr))
         ibpf['ip4_interest'][ctypes.c_ulong(socket.htonl(taddr))] = ctypes.c_bool(1)
 
-    if config.cmd:
-        cmd = config.cmd.format(config.host)
-        running = ' running {} ({});'.format(config.mode, cmd)
-        proc = subprocess.Popen(cmd.split(), shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    else:
-        cmd = ''
-        running = ''
-        proc = None
     logging.info("Installed ebpf code;{} ctrl+c to interrupt".format(running))
 
     outfile = open(config.outfile + ".csv", 'wb')
@@ -171,10 +148,6 @@ def main(config):
                 killit = True
                 break
 
-            if proc is not None and proc.poll() is not None:
-                logging.info("{} done; exiting".format(config.mode))
-                break
-
             nextresult = _get_nextresult()
             if nextresult != -1:
                 while resultidx != nextresult:
@@ -191,14 +164,6 @@ def main(config):
             killit = True
             logging.info("ctrl+c received; exiting")
             break
-
-    program_output = ""
-    if proc is not None:
-        if killit:
-            logging.info("killing process {} and waiting for it".format(proc.pid))
-            proc.kill()
-            proc.wait()
-        program_output = proc.stdout.read()
 
     logging.info("Waiting 1s for any stray responses")
     time.sleep(1.0)
@@ -225,31 +190,16 @@ def main(config):
     ip.tc('del', 'clsact', idx)
     ipdb.release()
     outfile.close()
-
-    extra = ""
-    if proc is not None:
-        logging.info("{} output: ".format(program_output))
-        with open(config.outfile + ".out", 'w') as outfile:
-            print(cmd, file=outfile)
-            outfile.write(program_output)
-        extra = " and {}.out".format(config.outfile)
-    logging.info("Output written to {}.csv{}".format(config.outfile, extra))
+    logging.info("Done.")
 
 
 if __name__ == '__main__':
-    #icmp_default = 'ping -c 10 -s 8 {}'
-    #tcp_default = 'ndtclt -b 134217728 -n {}'
-    #default_host = 'NDT-IUPUI-mlab1-lga03.measurement-lab.org' 
-
     parser = argparse.ArgumentParser('In-band measurement')
-    parser.add_argument('-c', '--cmd', default='',
-                        help='Command to use; no default; assumes that {} is included in the command for filling in the target host, which is specified with --host')
+    parser.add_argument('-c', default='someta_ebpf.c',
+                        help='File containing eBPF code to install')
     parser.add_argument('-d', '--debug', 
                         action='store_true', default=False,
                         help="Turning on some debugging output (incl. bpf_trace_printk messages")
-    parser.add_argument('--host', 
-                        default='',
-                        help='Target host for command (no default)')
     parser.add_argument('-b', '--bunch', default=False, action='store_true',
                         help="Turn on 'bunch' mode (default: off)")
     parser.add_argument('-e', '--noether', default=True, action='store_false',
@@ -258,10 +208,8 @@ if __name__ == '__main__':
                         help="Spacing between probes (or probe bunches) to each hop, in microseconds (default 10 millisec/10000 microsec)")
     parser.add_argument('-o', '--outfile', default='inband', 
                         help='File (basename) to write probe output data')
-    parser.add_argument('-t', '--maxttl', default=0, type=int,
+    parser.add_argument('-t', '--maxttl', default=16, type=int,
                         help="Set the max TTL value explicitly; otherwise use the number of hops from a traceroute to the destination")
-    parser.add_argument('-m', '--mode', default='icmp', choices=('icmp','tcp'),
-                        help='Application mode (icmp or tcp; default icmp/ping)')
     parser.add_argument('-i', '--device', type=str, required=True,
                         help='Network device name, e.g., eth0 to use')
     parser.add_argument('-T', '--timeout', default=0, 
