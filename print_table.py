6cdfb7d79573afd52c148f16e5d6c2547dc189e9
Joel Sommers <jsommers@colgate.edu>
2020-01-25 14:03:26 -0500

    update script for printing summary of collected data

--- print_table.py
+++ print_table.py
@@ -7,11 +7,18 @@ import pandas as pd
 with open(sys.argv[1]) as infile:
     d = json.load(infile)
 df = pd.read_json(json.dumps(d['results']))
-print(df['recvttl'].value_counts())
-print(df['outttl'].value_counts())
-print(df['protocol'].value_counts())
-print(df)
-
+nolat = len(df[df['latency'] == -1])
+print("No-latency rows: ", nolat, len(df), nolat/len(df), end="\n\n")
+df = df[df['latency'] != -1]
+print("recvttl value counts:", df['recvttl'].value_counts(), end="\n\n")
+print("outttl value counts:", df['outttl'].value_counts(), end="\n\n")
+cols = ['seq','latency','outttl','recvttl','protocol']
+print()
+print(df.head(10).to_string(columns=cols))
+print()
+print(df[cols].describe().to_string())
+print("\n")
 for seq in sys.argv[2:]:
-    print(seq, df[df['seq']==int(seq)])
+    print("seq", seq, df[df['seq']==int(seq)][cols])
+    print()
     
